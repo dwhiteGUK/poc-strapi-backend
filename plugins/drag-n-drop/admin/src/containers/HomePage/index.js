cf1949e89875f2e3dd8211adf583f841f49c9195
Darren White <dwhite@gentingcasinos.co.uk>
2021-04-08 15:43:00 +0100

    :construction: update drag styles

--- plugins/drag-n-drop/admin/src/containers/HomePage/index.js
+++ plugins/drag-n-drop/admin/src/containers/HomePage/index.js
@@ -5,10 +5,10 @@
  */
 
 import React, { memo, useEffect, useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-import { Table } from '@buffetjs/core';
 import { Header } from '@buffetjs/custom';
+import { GrabLarge } from '@buffetjs/icons';
 import { request } from 'strapi-helper-plugin';
 
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
@@ -16,13 +16,27 @@ import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 const Ul = styled.ul`
   list-style-type: none;
   margin: 0;
+  padding: 0;
 `;
 
 const Li = styled.li`
+  display: flex;
   list-style-type: none;
-  margin: 10px;
-  border: 1px dashed #292b2c;
+  margin: 10px 0;
   padding: 10px;
+  cursor: 'move',
+  background: ${({ theme, snapshot }) => theme.main.colors.white};
+  border-radius: ${({ theme }) => theme.main.sizes.borderRadius};
+  box-shadow: 0 2px 4px #e3e9f3;
+
+  ${props => props.snapshot.isDragging && css`
+    background: #DC2626;
+    color: white
+  `}
+`
+
+const Col = styled.span`
+  padding: 0 10px;
 `
 
 const HomePage = (props) => {
@@ -71,13 +85,23 @@ const HomePage = (props) => {
 
       <DragDropContext onDragEnd={handleOnDragEnd}>
         <Droppable droppableId="dragItems">
-          {(provided) => (
-            <Ul {...provided.droppableProps} ref={provided.innerRef}>
+          {(provided, snapshot) => (
+            <Ul
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
               {data.map((item, index) => (
                 <Draggable key={item.id} draggableId={`list-${item.id}`} index={index}>
-                  {(provided) => (
-                    <Li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                      {item.heading}
+                  {(provided, snapshot) => (
+                    <Li
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      snapshot={snapshot}
+                    >
+                      <Col><GrabLarge /></Col>
+                      <Col>{item.position ? item.position : '-'}</Col>
+                      <Col>{item.heading}</Col>
                     </Li>
                   )}
                 </Draggable>
